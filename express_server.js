8665c82e107a9569d034741ee384d99f213851f1
Jeff Gebert <jeffgebert@hotmail.com>
2019-10-29 03:02:37 +0000

    added login and logout button with cookies

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const app = express();
+app.use(cookieParser());
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -51,18 +53,20 @@ app.get("/set", (req, res) => {
  });
 
  app.get("/urls", (req, res) => {
-   console.log("test");
-   let templateVars = { urls: urlDatabase };
+
+   let templateVars = { urls: urlDatabase , username: req.cookies.username};
+   console.log(templateVars);
    res.render("urls_index", templateVars);
 
  });
 
  app.get("/urls/new", (req, res) => {
+   let templateVars = {username: req.cookies.username};
   res.render("urls_new");
 });
 
  app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies.username};
   res.render("urls_show", templateVars);
 });
 
@@ -91,6 +95,23 @@ app.post("/urls/:shortURL/update", (req, res) => {
   urlDatabase[req.params.shortURL]=req.body.longURL;
   res.redirect("/urls");
   
+
+});
+
+app.post("/login", (req, res) => {
+  console.log("hi")
+  
+  res.cookie("username", req.body.username); 
+  res.redirect("/urls");
+
+
+});
+
+app.post("/logout", (req, res) => {
+  console.log("log0ut")
+  res.clearCookie("username", req.body.username); 
+  res.redirect("/urls");
+
 
 });
 
