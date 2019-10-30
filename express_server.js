68365ea997794ee5179e06c39ae97d8754eadfb3
Jeff Gebert <jeffgebert@hotmail.com>
2019-10-30 15:02:52 +0000

    initialized branch for user-specific-features

--- express_server.js
+++ express_server.js
@@ -10,8 +10,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "aJ48lW"},
+  "9sm5xK": { longURL: "http://www.google.com", userID: "aJ48lW"}
 };
 
 const users = { 
